53972d56f21171ab77518e2eef37777c5e4facf7
Grigoris Chatzinikolaou <greghatzis@gmail.com>
2020-03-15 16:24:33 +0200

    Data atributes.

--- src/ajax-component.js
+++ src/ajax-component.js
@@ -1,6 +1,6 @@
 function AjaxComponent(DOMElement) {
 
-  if (DOMElement.length > 0) {
+  if (DOMElement) {
     this.DOMElement = DOMElement;
   }
 
@@ -9,7 +9,7 @@ function AjaxComponent(DOMElement) {
     urlParams: null,
     timeout: 5000,
     cacheResults: true,
-    timeouteMessage: "The request has timed out",
+    timeoutMessage: "The request has timed out",
     errorMessage: "Something went wrong",
     notReadyMessage: "AjaxComponent is still loading.",
   };
@@ -27,6 +27,9 @@ function AjaxComponent(DOMElement) {
   // Getters
   this.isLoading = function() {
     return this.state.loading;
+  }
+  this.isSuccessful = function() {
+    return this.state.success;
   }
   this.hasError = function() {
     return this.state.error;
@@ -59,57 +62,86 @@ function AjaxComponent(DOMElement) {
   
   this.renderError = null;
 
+  this.updateDOM = function(callback) {
+
+    // this.DOMElement.childNodes.forEach(el => {
+    //   if (el.)
+    // });
+    
+    // document.querySelectorAll('[data-if="isLoading"]').forEach(el => {
+    //   el.style.display = 'none';
+    // });
+    
+    // if (this.isLoading()) {
+      
+    // } else {
+    //   document.querySelectorAll('[data-if="isLoading"]').style.display = 'none';
+    // }
+
+    // if (this.isSuccessful()) {
+    //   document.querySelectorAll('[data-if="isSuccessful"]').style.display = 'block';
+    // } else {
+    //   document.querySelectorAll('[data-if="isSuccessful"]').style.display = 'none';
+    // }
+
+    // if (this.hasError()) {
+    //   document.querySelectorAll('[data-if="hasError"]').style.display = 'block';
+    // } else {
+    //   document.querySelectorAll('[data-if="hasError"]').style.display = 'none';
+    // }
+
+    if (callback instanceof Function) callback();
+  }
+
   this.update = function(params) {
    this.makeRequest('GET', params); 
   }
 
   this.makeRequest = function(method, params) {
     const comp = this;
-    if (comp.isLoading === false) {
+    if (comp.isLoading() === false) {
       if (params) this.updateParams(params);
 
-      if (comp.renderLoading instanceof Function) {
-        comp.reset();
-        comp.state.loading = true;
-        comp.renderLoading(function() {
+      comp.reset();
+      comp.state.loading = true;
+      comp.updateDOM(function() {
 
-          $.ajax({
-            url: comp.settings.baseUrl, data: comp.settings.urlParams,
-            method: method, dataType: 'json',
-            cache: comp.settings.cacheResults, timeout: comp.timeout,
-            success: function(response) {
-              comp.state.success = true;
-              comp.data = response;
-            },
-            error: function(jqXHR, status, message) {
-              comp.state.error = true;
-              if (jqXHR.responseJSON && jqXHR.responseJSON.message) {
-                comp.state.message = jqXHR.responseJSON.message;
+        $.ajax({
+          url: comp.settings.baseUrl, data: comp.settings.urlParams,
+          method: method, dataType: 'json',
+          cache: comp.settings.cacheResults, timeout: comp.settings.timeout,
+          success: function(response) {
+            comp.state.success = true;
+            comp.data = response;
+          },
+          error: function(jqXHR, status, message) {
+            comp.state.error = true;
+            if (jqXHR.responseJSON && jqXHR.responseJSON.message) {
+              comp.state.message = jqXHR.responseJSON.message;
+            } else {
+              if (status === 'timeout') {
+                comp.state.message = comp.settings.timeoutMessage;
+              } else if (message) {
+                comp.state.message = message;
               } else {
-                if (status === 'timeout') {
-                  comp.state.message = this.settings.timeouteMessage;
-                } else if (message) {
-                  comp.state.message = message;
-                } else {
-                  comp.state.message = this.settings.errorMessage;
-                }
+                comp.state.message = comp.settings.errorMessage;
               }
-              comp.renderError();
-            },
-            complete: function() {
-              comp.state.loading = false;
-              comp.renderDefault();
             }
-          });
-
+            comp.renderError();
+          },
+          complete: function() {
+            comp.state.loading = false;
+            comp.updateDOM(callback)
+            // if (comp.hasError() === false) comp.renderDefault();
+          }
         });
-      } else {
-        throw "renderLoading must be a function that accepts a callback.";
-      }
+
+      });
       
     } else {
       throw comp.settings.notReadyMessage;
     }
     
   }
+
 }
